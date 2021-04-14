0ac4e2429a683065c77e3ef85d0c7de4d568781f
Rafael Sakamoto <rafael.sakamoto@gmail.com>
2021-04-14 10:23:50 -0300

    correção na classe Invetimento

--- src/com/company/Investimento.java
+++ src/com/company/Investimento.java
@@ -6,4 +6,46 @@ public class Investimento {
     private String modalidade;
     private double rendimento;
 
+    public double getValor() {
+        return valor;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+
+    public double getRendimento() {
+        return rendimento;
+    }
+
+    public void setValor(double valor) {
+        if(valor < conta.getValor()) {
+            this.valor = valor;
+        }
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+
+    public void setRendimento(double rendimento) {
+        this.rendimento = rendimento;
+    }
+
+    public Investimento(double valor, Conta conta, String modalidade, double rendimento) {
+        if(valor < conta.getValor()) {
+            this.valor = valor;
+        }
+        this.conta = conta;
+        this.modalidade = modalidade;
+        this.rendimento = rendimento;
+    }
 }
